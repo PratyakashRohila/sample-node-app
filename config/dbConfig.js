0fb5fe0f0b85dc81b23847e1e87d2ea53edac6f8
PratyakashRohila <Pratyakash.Rohila@cubastion.com>
2024-09-14 13:53:16 +0530

    Update dbConfig.js

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -5,7 +5,7 @@ let config = dotenv.parsed
 module.exports = {
   production: {
     username: process.env.DBUSERNAME ? process.env.DBUSERNAME : config?.DBUSERNAME,
-    password: process.env.DBPASSWORD ? process.env.DBPASSWORD : config?.PASSWORD,
+    password: process.env.DBPASSWORD ? process.env.DBPASSWORD : config?.DBPASSWORD,
     database: process.env.DBNAME ? process.env.DBNAME : config?.DBNAME,
     host: process.env.DBHOST ? process.env.DBHOST : config?.DBHOST,
     port: process.env.DBPORT ? process.env.DBPORT : config?.DBPORT,
